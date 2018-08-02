39bc88415950f728ae29a3e43c9091fce3a7a432
Sujoy Datta <sujoydatta26@gmail.com>
2018-08-02 17:42:11 +0530

    Initial commit

---
+++ app/src/androidTest/java/com/morningstar/cerebro/ExampleInstrumentedTest.java
@@ -0,0 +1,34 @@
+/*
+ * Created by Sujoy Datta. Copyright (c) 2018. All rights reserved.
+ *
+ * To the person who is reading this..
+ * When you finally understand how this works, please do explain it to me too at sujoydatta26@gmail.com
+ * P.S.: In case you are planning to use this without mentioning me, you will be met with mean judgemental looks and sarcastic comments.
+ */
+
+package com.morningstar.cerebro;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.morningstar.cerebro", appContext.getPackageName());
+    }
+}
